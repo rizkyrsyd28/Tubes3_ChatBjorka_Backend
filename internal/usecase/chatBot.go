c57360094bbc2da91d0c83990d3b16a9dccf19f6
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-05-05 11:31:58 +0700

    edit Generate answer for test

--- internal/usecase/chatBot.go
+++ internal/usecase/chatBot.go
@@ -11,7 +11,10 @@ type ChatBotUseCase interface {
 }
 
 func (u usecase) GenerateAnswer(c context.Context, idTitle string, idUser string, input entity.UserInput) (entity.BotOutput, error) {
-	var result entity.BotOutput = entity.BotOutput{Message: "def", IDTitle: "def"}
+	var result entity.BotOutput = entity.BotOutput{
+		Message: "Maecenas et tellus ipsum. Curabitur gravida enim vel tellus iaculis aliquam. Nullam volutpat, quam in sagittis laoreet, odio risus viverra risus, sed convallis ante risus et velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam blandit elementum massa, non tempor augue luctus quis. Pellentesque eu velit non elit pulvinar ultrices quis aliquam justo. Proin id nulla volutpat velit semper porta.",
+		IDTitle: "def",
+	}
 	title, err := u.repo.GetHistoryById(c, idTitle, idUser)
 	fmt.Println(title.IDTitle + " " + title.Title + " " + title.IDUser)
 	if title.Title != "" {
@@ -24,6 +27,7 @@ func (u usecase) GenerateAnswer(c context.Context, idTitle string, idUser string
 	for _, data := range datas {
 		fmt.Printf("id_qna : %d, q : %s, a : %s\n", data.IDQna, data.Question, data.Answer)
 	}
+	u.repo.AddChat(c, idTitle, input.Message, result.Message)
 	result.IDTitle = idTitle
 	err = u.repo.AddData(c, "Ini pertanyaan", "Ini Jawaban")
 	return result, err
